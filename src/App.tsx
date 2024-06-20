03ddc58aac07d3e1221aeff356653c977e5203a3
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-06-20 09:29:23 +0200

    Created a main and App files and used react routing app pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './globals.css';
+import { Routes, Route } from 'react-router-dom';
 
+import './globals.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className='text-3x1 font-bold underline'>
+      InstaScape
+    </h1>
   )
 }
 
