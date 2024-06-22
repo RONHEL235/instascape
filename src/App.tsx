d94914ae2d3095c3f79657c6a64144a205997bfb
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-06-22 08:22:30 +0200

    Placed Singin and Singout pages inside Authentication Layout page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 
-import './globals.css';
 import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
 import { Home } from './_root/pages';
+import './globals.css';
 
 const App = () => {
   return (
     <main className='flex h-screen'>
       <Routes>
-        //Public routes 
-        <Route path ="/sign-in" element={<SigninForm />} />
+        //Public routes
+        <Route element={<AuthLayout />}>
+          <Route path ="/sign-in" element={<SigninForm />} />
+          <Route path ="/sign-in" element={<SignupForm />} />
+        </Route> 
         //Private Routes
         <Route index element={<Home />} />
       </Routes>
