562a3b2daee1f3ba83beb2f297af6853554a5a39
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-06-29 09:52:32 +0200

    Added a public folder to add all images for the sign in and sign up pages.

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -10,7 +10,12 @@ import { Outlet, Navigate } from "react-router-dom"
         <Navigate to="/" /> 
       ): (
         <>
-          <section>
+        <img
+            src="/assets/images/sign-up.jpg" 
+            alt="logo"
+            className="xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
         </>
