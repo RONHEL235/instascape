62a095ff577a29a29d7d4785a8d9caedfd8815c3
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-06-30 08:27:41 +0200

    Extracted formschema into a separate validations file.

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,27 +1,24 @@
 import { Button } from "@/components/ui/button"
-import { z } from "zod"
 
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { SignupValidation } from "@/lib/validation"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
+import { z } from "zod"
 
 const SignupForm = () => {
 
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
       username: "",
     },
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,5 @@
+import { z } from "zod"
+
+export const SignupValidation = z.object({
+    username: z.string().min(2).max(50),
+})
