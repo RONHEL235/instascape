486b44c351e75fda92acff09b8b9eb1a5837a2ce
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-01 07:57:57 +0200

    Added a logo on the sign up form page.

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -29,7 +29,7 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img />        
+        <img src="/assets/images/InstascpaLogo2.svg" alt="logo"/>        
       </div>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
