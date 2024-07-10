2580f188e8fe2dec704752c53dc9011dbed984c4
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-10 16:15:44 +0200

    Installed react-query for simplifying data fetching and mutation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
-
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
-import AuthLayout from './_auth/AuthLayout';
-import { Home } from './_root/pages';
-import './globals.css';
-import RootLayout from './_root/RootLayout';
+import { Routes, Route } from 'react-router-dom'
+import { Toaster } from "@/components/ui/toaster"
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import AuthLayout from './_auth/AuthLayout'
+import { Home } from './_root/pages'
+import './globals.css'
+import RootLayout from './_root/RootLayout'
 
 const App = () => {
   return (
@@ -21,6 +21,8 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+
+      <Toaster />
     </main>
   )
 }
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,15 +1,17 @@
 import Loader from "@/components/shared/Loader"
 import { Button } from "@/components/ui/button"
 import { Link } from "react-router-dom"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { SignupValidation } from "@/lib/validation"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { createUserAccount } from "@/lib/appwrite/api"
+import { useToast } from "@/components/ui/use-toast"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
 
 const SignupForm = () => {
+  const { toast } = useToast()
   const isLoading = false
 
   // 1. Define your form.
@@ -25,11 +27,13 @@ const SignupForm = () => {
  
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
-    const newUser = await createUserAccount(values)
+    const newUser = await createUserAccount(values) 
     
     if (!newUser) {
-      return       
+      return toast({
+        title: "Sign up failed. Please try again."})      
     }
+    //const session = await signInAccount() 
   }
   return (
     <Form {...form}>
