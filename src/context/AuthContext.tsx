99256f39f97bbe8c8748e243875349f94a3782a5
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-11 05:39:00 +0200

    Storing session in react context for a logged user.

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,11 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+
+
+/*After creating the session, it has to be stored in react context to know if we have a logged in user or not*/
+function AuthContext() {
+  return ( 
+    <div>AuthContext</div>
+  )
+}
+
+export default AuthContext
