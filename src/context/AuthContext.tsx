e2859e41d1b75606fbebe628649772fc1945e2eb
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-11 11:18:23 +0200

    Implemented the function CheckAuthUser to check if user is logged in or not.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,86 @@
+import { getCurrentUser } from '@/lib/appwrite/api'
+import { IUser } from '@/types'
 import { createContext, useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
+/*After creating the session, it has to be stored in react context to know if we have a logged in user or not.*/
 
-/*After creating the session, it has to be stored in react context to know if we have a logged in user or not*/
-function AuthContext() {
+export const INITIAL_USER = {
+    id: "",
+    name: "",
+    username: "",
+    email: "",
+    imageUrl: "",
+    bio: "",
+}
+
+//This is to know if there is a logged in user at all times.
+const INITIAL_STATE = {
+    user: INITIAL_USER,
+    isLoading: false,
+    isAuthenticated: false,
+    setUSer: () => {},
+    setIsAuthenticated: () => {},
+    checkAuthUser: async () => false as boolean,
+}
+
+const AuthContext = createContext<IContextType>(INITIAL_STATE)  
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<IUser>(INITIAL_USER)
+    const [isLoading, setIsLoading] = useState(false)
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+    const navigate = useNavigate()
+
+    const checkAuthUser = () => {
+        try {
+            const currentAccount = await getCurrentUser()
+
+            if(currentAccount) {
+                setUser({
+                    id: currentAccount.$id,
+                    name: currentAccount.name,
+                    username: currentAccount.username,
+                    email: currentAccount.email,
+                    imageUrl: currentAccount.imageUrl,
+                    bio: currentAccount.bio,
+                })
+
+                setAuthenticated(true)
+                
+                return true
+            }
+
+            return false
+        } catch (error) {
+            console.log(error)
+            return false
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        if(
+            localStorage.getItem("cookieFallback") === "[]" ||
+            localStorage.getItem("cookieFallback") === null
+        ) navigate("/sign-in")
+
+        checkAuthUser()
+    }, [] )  
+    const value = {
+        user,
+        setUser,
+        isLoading,
+        isAuthenticated,
+        setIsAuthenticated,
+        checkAuthUser,
+    }    
   return ( 
-    <div>AuthContext</div>
+    <AuthContext.Provider value={value}>
+        {children}
+    </AuthContext.Provider>
   )
 }
 
