73c1c14e5e82a2c46fb6d61361502006afe181d6
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-12 06:18:21 +0200

    Done with topbar for mobile responsiveness.

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -31,7 +31,11 @@ const Topbar = () => {
                     <h3>Logout</h3> 
                 </Button>
                 <Link to={`/profile/${user.id}`} className="flex-center gap-3">
-                
+                    <img 
+                    src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+                    alt="profile"
+                    className="h-8 w-8 rounded-full"
+                    />
                 </Link>
             </div>
         </div>
