9f94f145eaedfba574d9b07b957d53ced9b04c1b
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-12 11:08:29 +0200

    Added all routes to sub pages within application.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster"
 import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
 import AuthLayout from './_auth/AuthLayout'
-import { Home } from './_root/pages'
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages'
 import './globals.css'
 import RootLayout from './_root/RootLayout'
 
@@ -19,6 +19,14 @@ const App = () => {
         //Private Routes
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
---
+++ src/_root/pages/LikedPosts.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LikedPosts() {
+  return (
+    <div>LikedPosts</div>
+  )
+}
+
+export default LikedPosts
