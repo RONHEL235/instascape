0adf58e0dca867c68eea2906fb5e70ee5cc63413
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-07-12 10:24:17 +0200

    Added five more icons to use in the sidebar page.

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { Link, NavLink, useNavigate, useLocation } from 'react-router-dom'
 import { Button } from '../ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
 import { useEffect } from 'react'
@@ -7,6 +7,7 @@ import { sidebarLinks } from '@/constants'
 import { INavLink } from '@/types'
 
 const LeftSidebar = () => {
+    const { pathname } = useLocation()
     const { mutate: signOut, isSuccess } = useSignOutAccount()
     const navigate = useNavigate()
     const { user } = useUserContext()
@@ -45,6 +46,7 @@ const LeftSidebar = () => {
 
         <ul className="flex flex-col gap-6">
           {sidebarLinks.map((link: INavLink) => {
+            const isActive = pathname === link.route
             return (
               <li key={link.label} 
               className="leftsidebar-link">
@@ -52,6 +54,11 @@ const LeftSidebar = () => {
                  to={link.route}
                  className="flex gap-4 items-center p-4"
                 >
+                  <img
+                  src={link.imgURL}
+                  alt={link.label}
+                  className="group-hover:invert-white" 
+                  />
                   {link.label}
                 </NavLink>
               </li>
